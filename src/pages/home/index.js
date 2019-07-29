d2c0938b5078975d14d0a705fbe3c5ec8742c39d
ddguru777 <ddguru777@gmail.com>
2019-07-29 15:57:13 -0400

    copy nav sidebar stylesheet

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { Helmet } from "react-helmet"
 
+import "./home.scss"
+
 class Home extends React.Component {
   render() {
     return (
@@ -8,7 +10,62 @@ class Home extends React.Component {
         <Helmet>
           <title>Dashboard</title>
         </Helmet>
-        <h1>Home!!!</h1>
+        <div id="wrapper">
+          <nav className="navbar-default navbar-static-side" role="navigation">
+            <div className="sidebar-collapse">
+              <ul className="nav metismenu" id="side-menu">
+                <li className="nav-header">
+                  <div className="dropdown profile-element">
+                    <span className="clear">
+                      <span className="block m-t-xs">
+                        <strong className="font-bold">user@example.com</strong>
+                      </span>
+                    </span>
+                  </div>
+                </li>
+                <li>
+                  <a href="index.html">
+                    <i />
+                    Capabilities List
+                  </a>
+                </li>
+                <li className="active">
+                  <a href="minor.html">
+                    <i /> Settings
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+
+          <div id="page-wrapper" className="gray-bg">
+            <div className="row border-bottom">
+              <nav
+                className="navbar navbar-static-top white-bg"
+                role="navigation"
+              >
+                <ul className="nav navbar-top-links navbar-right">
+                  <li>
+                    <a href="#">
+                      <i />
+                      Log out
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+
+            <div className="wrapper wrapper-content animated fadeInRight">
+              <div className="row">
+                <div className="col-lg-12">
+                  <div className="text-center m-t-lg">
+                    <h1>Welcome!</h1>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </dv>
     )
   }
