f7f5c39a3a450803df2e3bfa42813132bc5bfaee
ddguru777 <ddguru777@gmail.com>
2019-07-30 12:57:57 -0400

    layout

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,72 +1,29 @@
 import React from "react"
 import { Helmet } from "react-helmet"
 
-import "./home.scss"
+import styles from "./home.module.scss"
+import classnames from "classnames"
 
 class Home extends React.Component {
   render() {
     return (
-      <dv>
+      <div>
         <Helmet>
           <title>Dashboard</title>
         </Helmet>
-        <div id="wrapper">
-          <nav className="navbar-default navbar-static-side" role="navigation">
-            <div className="sidebar-collapse">
-              <ul className="nav metismenu" id="side-menu">
-                <li className="nav-header">
-                  <div className="dropdown profile-element">
-                    <span className="clear">
-                      <span className="block m-t-xs">
-                        <strong className="font-bold">user@example.com</strong>
-                      </span>
-                    </span>
-                  </div>
-                </li>
-                <li>
-                  <a href="index.html">
-                    <i />
-                    Capabilities List
-                  </a>
-                </li>
-                <li className="active">
-                  <a href="minor.html">
-                    <i /> Settings
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </nav>
-
-          <div id="page-wrapper" className="gray-bg">
-            <div className="row border-bottom">
-              <nav
-                className="navbar navbar-static-top white-bg"
-                role="navigation"
-              >
-                <ul className="nav navbar-top-links navbar-right">
-                  <li>
-                    <a href="#">
-                      <i />
-                      Log out
-                    </a>
-                  </li>
-                </ul>
-              </nav>
-            </div>
-
-            <div className="wrapper wrapper-content animated fadeInRight">
-              <div className="row">
-                <div className="col-lg-12">
-                  <div className="text-center m-t-lg">
-                    <h1>Welcome!</h1>
-                  </div>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col pl-0 pr-0">
+              <div className="container-flex">
+                <div className={classnames(styles.navbar)}>ddddf</div>
+                <div className={styles.content}>
+                  <h1>Page Content</h1>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </dv>
+      </div>
     )
   }
 }
